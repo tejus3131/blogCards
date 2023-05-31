536ce4b1fbfe3c5e0051b71c76e5d37a96edd742
Tejus Gupta <tejus3131@gmail.com>
2023-06-01 01:26:11 +0530

    final commit

--- index.js
+++ index.js
@@ -1,82 +1,127 @@
 const cardHolder = document.querySelector('#card-holder')
 const cardTitle = document.querySelector('#title');
 const cardDesc = document.querySelector('#description');
-const cardSource = document.querySelector('#source');
+const cardLink = document.querySelector('#link');
 const addCardBtn = document.querySelector('#add-card');
+const source = document.querySelector('#source');
+const filedata = document.querySelector('#fileName');
+const dialog = document.querySelector('#favDialog');
+const bg = document.querySelector('#bg');
+const add = document.querySelector('#add');
+const addDialog = document.querySelector('#cardDialog');
+const head = document.querySelector('#head');
 
+var blogs = 0;
 
-let blogs = [];
+function toggle_error() {
+    dialog.showModal();
+    bg.classList.add("blur");
 
-function renderCards() {
-    if (blogs.length == 0) {
-        cardHolder.innerHTML = `No Blogs Added Yet`;
-    }
-    else {
-        cardHolder.innerHTML = ``;
-        blogs.forEach((card, index) => {
-            const cardItem = document.createElement('li');
-            cardHolder.classList.add('card-item');
-            cardItem.innerHTML = `
-            <div class="card">
-                <img name="image" class="image" src="${card.source}" />
-                <label for="image" class="head"><strong>${card.title}</strong></label>
-                <label for="image" class="desc">${card.desc}</label>
-                <div class="commands">
-                    <button class="btn edit" data-index="${index}">Edit</button>
-                    <button class="btn delete" data-index="${index}">Delete</button>
-                </div>
-            </div>
-            `;
-            cardHolder.appendChild(cardItem);
-        })
-    }
 }
 
-function addCard() {
+function newCard() {
+    addDialog.showModal();
+    bg.classList.add("blur");
+}
+
+function close_error() {
+    dialog.close();
+    bg.classList.remove("blur");
+    newCard();
+}
+
+function addNewCard() {
     const title = cardTitle.value;
     const desc = cardDesc.value;
-    const source = cardSource.value;
-    const card = {
-        title: title,
-        desc: desc,
-        source: source
-    };
-    blogs.push(card);
-    cardTitle.value = "";
-    cardDesc.value = "";
-    cardSource.value = "";
-    console.log(blogs)
-    renderCards();
+    const file = source.files[0];
+    const link = cardLink.value;
+    index = Math.floor(Math.random() * (1000000000 - 9999999999)) + 1000000000;
+    if (title == "") {
+        toggle_error();
+        return;
+    }
+    if (desc == "") {
+        toggle_error();
+        return;
+    }
+    if (link == "") {
+        toggle_error();
+        return;
+    }
+    if (source.value == "") {
+        toggle_error();
+        return;
+    }
+    const img = window.URL.createObjectURL(file);
+    const cardItem = document.createElement('li');
+    cardHolder.classList.add('card-item');
+    cardItem.innerHTML = `
+        <div class="card" id="${index}">
+            <img name="image" class="image" src="${img}" />
+            <label for="image" class="head"><strong>${title}</strong></label>
+            <label for="image" class="desc">${desc}</label>
+            <div class="commands">
+                <button class="btn delete" data-index="${index}">Delete</button>
+                <button class="btn" type="button" onclick="window.open('${link}', '_blank')" >check</button>
+            </div>
+        </div>
+        `;
+    cardHolder.appendChild(cardItem);
+    blogs += 1;
+    reset()
 }
 
 function deleteCard(index) {
-    blogs.splice(index, 1);
-    renderCards();
+    var elem = document.getElementById(index);
+    elem.parentNode.removeChild(elem);
+    blogs -= 1;
+    reset();
 }
 
-function editCard(index) {
-    const title = cardTitle.value;
-    const desc = cardDesc.value;
-    const source = cardSource.value;
-    const card = {
-        title,
-        desc,
-        source
-    }
-    blogs.splice(index, 1, card);
-    renderCards();
-}
+addCardBtn.addEventListener('click', addNewCard);
 
-addCardBtn.addEventListener('click', addCard);
+add.addEventListener('click', newCard);
 
 cardHolder.addEventListener('click', (e) => {
     if (e.target.classList.contains('delete')) {
         deleteCard(e.target.dataset.index);
     }
-    if (e.target.classList.contains('edit')) {
-        editCard(e.target.dataset.index);
-    }
 });
 
+source.addEventListener('change', (e) => {
+    if (e.target.files.length == 0) {
+        alert("Please Select A Image");
+        return;
+    }
+    filedata.innerHTML = `Image Selected`;
+});
 
-renderCards();
+function reset() {
+    cardTitle.value = "";
+    cardDesc.value = "";
+    source.value = "";
+    cardLink.value = "";
+    filedata.innerHTML = `Select Image`;
+    addDialog.close()
+    bg.classList.remove("blur");
+    if (blogs == 0) {
+        head.classList.add("show")
+        head.classList.remove("hide")
+    }
+    else {
+        head.classList.remove("show")
+        head.classList.add("hide")
+    }
+
+}
+
+document.addEventListener('keyup', function (event) {
+    if (event.key == "Escape") {
+        dialog.close();
+        addDialog.close();
+        bg.classList.remove("blur");
+    }
+})
+
+reset();
+
